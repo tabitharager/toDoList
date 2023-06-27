c9459644c6ff8a91de3166f81d16d4d0a2715a89
tabitharager <06tabitha.rager@gmail.com>
2023-06-27 10:45:05 -0600

    Initial Commit

---
+++ hardApp/hardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  hardAppApp.swift
+//  hardApp
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct hardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
